22aea46cba2697a901bfa151a0236de28fbf321b
Loann <courcoul66@gmail.com>
2022-07-06 08:46:02 +0200

    Start circle on select bat

--- Assets/Scripts/Player/BatimentPrincipal.cs
+++ Assets/Scripts/Player/BatimentPrincipal.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class BatimentPrincipal : MonoBehaviour
 {
     [SerializeField] private LayerMask layerBatPrincipal;
-
+    [SerializeField] private LayerMask layerOtherBat;
+    [SerializeField] private GameObject circleToSpawnUnderBatPrincipal, circleToSpawnUnderOtherBat;
+    
     private GameObject _goBatPrincipal;
     private bool _isBatPrincipalTouched;
     private Camera _cam;
@@ -15,6 +18,11 @@ public class BatimentPrincipal : MonoBehaviour
     private void Start()
     {
         _cam = Camera.main;
+        var position = transform.position;
+        circleToSpawnUnderBatPrincipal = Instantiate(circleToSpawnUnderBatPrincipal, position, Quaternion.Euler(-90,0,0));
+        circleToSpawnUnderBatPrincipal.SetActive(false);
+        circleToSpawnUnderOtherBat = Instantiate(circleToSpawnUnderOtherBat, position, Quaternion.Euler(-90,0,0));
+        circleToSpawnUnderOtherBat.SetActive(false);
     }
 
     // Update is called once per frame
@@ -22,11 +30,22 @@ public class BatimentPrincipal : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
+            Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
             // Does the ray intersect any objects excluding the player layer
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out var hit, Mathf.Infinity, layerBatPrincipal))
+            if (Physics.Raycast(ray, out var hit, Mathf.Infinity, layerBatPrincipal))
             {
+                var hitPos = hit.transform.position;
+                circleToSpawnUnderBatPrincipal.transform.position = new Vector3(hitPos.x, 0.01f, hitPos.z);
+                circleToSpawnUnderBatPrincipal.SetActive(true);
                 _goBatPrincipal = hit.collider.gameObject;
-                _isBatPrincipalTouched = true;
+                _isBatPrincipalTouched = true;  
+            }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            if (circleToSpawnUnderBatPrincipal != null)
+            {
+                circleToSpawnUnderBatPrincipal.SetActive(false);
             }
         }
 
@@ -36,10 +55,26 @@ public class BatimentPrincipal : MonoBehaviour
             {
                 Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
 
-                if (Physics.Raycast(_goBatPrincipal.transform.position, ray.direction, out var hit))
+                if (Physics.Raycast(ray, out var hit, Mathf.Infinity, layerOtherBat))
                 {
-                  Debug.Log(hit.collider.name);
+                    var hitPos = hit.transform.position;
+                    circleToSpawnUnderOtherBat.transform.position = new Vector3(hitPos.x, 0.01f, hitPos.z); 
+                    circleToSpawnUnderOtherBat.SetActive(true);
                 }
+                else
+                {
+                    if (circleToSpawnUnderOtherBat != null)
+                    {
+                        circleToSpawnUnderOtherBat.SetActive(false);
+                    }
+                }
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                if (circleToSpawnUnderOtherBat != null)
+                {
+                    circleToSpawnUnderOtherBat.SetActive(false);
+                }   
             }
         }
         
